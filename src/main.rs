5726c09948a5b1edb12252d54ef3319d2bc95aa9
Sebastian Korfmann <sebastian@korfmann.net>
2023-01-15 23:14:53 +0100

    Provide global awssdk config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use deno_core::OpState;
 use deno_core::error::AnyError;
 use deno_core::op;
 use deno_core::Extension;
@@ -8,6 +9,7 @@ use deno_runtime::permissions::Permissions;
 use deno_runtime::worker::MainWorker;
 use deno_runtime::worker::WorkerOptions;
 use deno_runtime::BootstrapOptions;
+use std::cell::RefCell;
 use std::path::Path;
 use std::rc::Rc;
 use std::sync::Arc;
@@ -30,13 +32,18 @@ fn op_hello_reverse(input: String) -> Result<String, AnyError> {
 }
 
 #[op]
-async fn op_aws_s3_list_buckets_async() -> Result<Vec<String>, AnyError> {
-    println!("Hello World of S3 LIst Buckets");
-    let config = aws_config::load_from_env().await;
-    let client = s3::Client::new(&config);
+async fn op_aws_s3_list_buckets_async(state: Rc<RefCell<OpState>>) -> Result<Vec<String>, AnyError> {
+    let client = {
+        // state needs to be dropped before client is used. Otherwise, the mutable borrow in deno_ffi will fail.
+        // e.g. "thread 'main' panicked at 'already borrowed: BorrowMutError'"
+        let opstate_ = state.borrow();
+        let config = opstate_.borrow::<aws_config::SdkConfig>();
+        let client = s3::Client::new(config);
+        client
+    };
     let resp = client.list_buckets().send().await?;
-    // return vec of bucket names
-    Ok(resp.buckets.unwrap().iter().map(|b| b.name.clone().unwrap()).collect::<Vec<String>>())
+    let result = resp.buckets.unwrap().iter().map(|b| b.name.clone().unwrap()).collect::<Vec<String>>();
+    Ok(result)
 }
 
 #[tokio::main]
@@ -49,8 +56,14 @@ async fn main() -> Result<(), AnyError> {
         todo!("Web workers are not supported in the example");
     });
 
+    let config = aws_config::load_from_env().await;
+
     let runjs_extension = Extension::builder()
         .ops(vec![op_hello_world::decl(), op_hello_reverse::decl(), op_aws_s3_list_buckets_async::decl()])
+        .state(move |state| {
+          state.put::<aws_config::SdkConfig>(config.clone());
+          Ok(())
+         })
         .build();
 
     println!("ARGC: {}", op_hello_reverse::decl().argc);
