b4d372cd1f3db1968cf00e0ea201e7fff4a46cab
Sebastian Korfmann <sebastian@korfmann.net>
2023-01-21 22:30:07 +0100

    Make Lambda Rust Runtime work - see #1

--- cloud/lib/rust-runtime/Readme.md
+++ cloud/lib/rust-runtime/Readme.md
@@ -3,6 +3,13 @@
 - https://dev.to/ryands17/rust-on-lambda-using-the-cdk-3ccm
 - https://words.filippo.io/easy-windows-and-linux-cross-compilers-for-macos/
 - https://burgers.io/cross-compile-rust-from-arm-to-x86-64
+- https://github.com/denoland/deno/issues/13458
+- https://choubey.gitbook.io/internals-of-deno/import-and-ops/5.6-registration-of-ops
+- https://github.com/ddprrt/lambda-hyper-example-workspace/blob/main/lambda/src/
+- https://github.com/awslabs/aws-lambda-rust-runtime/discussions/405
+- https://github.com/awslabs/aws-lambda-rust-runtime/issues/317#issuecomment-916226693
+- https://github.com/awslabs/aws-lambda-rust-runtime/issues/310
+lambda_event_layer.rs
 
 ```
 rustup target add x86_64-unknown-linux-musl
--- cloud/lib/rust-runtime/src/main.rs
+++ cloud/lib/rust-runtime/src/main.rs
@@ -1,30 +1,26 @@
 use std::path::Path;
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
-use byt_runtime::myworker::execute_module;
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
 
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code examples in the Runtime repository:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
-// Extract some useful information from the request
-    let sdk_config = aws_config::load_from_env().await;
-    let js_path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("examples")
-        .join("hello.js");
-    let main_module = deno_core::resolve_path(&js_path.to_string_lossy()).unwrap();
-    execute_module(main_module, sdk_config, Default::default()).await?;
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body("Hello AWS Lambda HTTP request".into())
-        .map_err(Box::new)?;
-    Ok(resp)
+/// This is also a made-up example. Requests come into the runtime as unicode
+/// strings in json format, which can map to any structure that implements `serde::Deserialize`
+/// The runtime pays no attention to the contents of the request payload.
+#[derive(Deserialize)]
+struct Request {
+    command: String,
 }
+
+/// This is a made-up example of what a response structure may look like.
+/// There is no restriction on what it can be. The runtime requires responses
+/// to be serialized into json. The runtime pays no attention
+/// to the contents of the response payload.
+#[derive(Serialize)]
+struct Response {
+    req_id: String,
+    msg: String,
+}
+
+use byt_runtime::myworker::execute_module;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -34,5 +30,29 @@ async fn main() -> Result<(), Error> {
         .without_time()
         .init();
 
-    run(service_fn(function_handler)).await
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+
+pub(crate) async fn my_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    // extract some useful info from the request
+    let command = event.payload.command;
+
+    let sdk_config = aws_config::load_from_env().await;
+    let js_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join("hello.js");
+    let main_module = deno_core::resolve_path(&js_path.to_string_lossy()).unwrap();
+    execute_module(main_module, sdk_config, Default::default()).await.unwrap();
+
+    // prepare the response
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Command {} executed.", command),
+    };
+
+    // return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
 }
